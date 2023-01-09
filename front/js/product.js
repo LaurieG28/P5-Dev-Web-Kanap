5c547a3f90fb65a129c4a6b11db8b44ad042cf4c
Laurie Gazelle <laurie.gazelle@gmail.com>
2023-01-09 12:42:21 +0100

    product.js + product.html

---
+++ front/js/product.js
@@ -0,0 +1,46 @@
+const url = new URL(window.location.href);
+const id = url.searchParams.get("id");
+
+fetch('http://localhost:3000/api/products/' + id)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+    })
+
+    .then(function (jsonPromise) {
+        const product = jsonPromise
+        console.log(jsonPromise);
+
+        const itemImage = document.querySelector(".item__img");
+        const image = document.createElement("img");
+
+        itemImage.appendChild(image);
+        image.src = product.imageUrl;
+        image.alt = product.altTxt;
+
+        const titleElement = document.getElementById("title");
+        titleElement.innerText = product.name;
+
+        const priceElement = document.getElementById("price");
+        priceElement.innerText = product.price;
+
+        const descriptionElement = document.getElementById("description");
+        descriptionElement.innerText = product.description;
+
+        const colorsElement = document.getElementById("colors");
+        const colors = product.colors;
+
+        for (const color of colors) {
+            const optionElement = document.createElement("option");
+            optionElement.innertext = product.color;
+            colorsElement.appendChild(optionElement);
+        }
+    })
+
+    .catch(function (error) {
+        console.log(error);
+    })
+
+
+
