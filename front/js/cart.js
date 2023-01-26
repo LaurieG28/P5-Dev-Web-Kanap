26b0b599e6b50a3ee2a557276a500c9511a6c265
Laurie Gazelle <laurie.gazelle@gmail.com>
2023-01-26 12:13:11 +0100

    add form

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,4 @@
 let cart = JSON.parse(window.localStorage.getItem("cart"));
-console.log(cart);
 
 let totalQuantityCart = 0;
 let totalPriceCart = 0;
@@ -143,9 +142,102 @@ if (cart !== null) {
                 spanQuantity.innerText = totalQuantityCart;
                 let spanPrice = document.getElementById("totalPrice");
                 spanPrice.innerText = totalPriceCart;
+
             });
     }
 }
+
+let inputFirstName = document.getElementById("firstName");
+let pFirstNameError = document.getElementById("firstNameErrorMsg");
+
+inputFirstName.addEventListener("input", function (event) {
+    event.preventDefault();
+    let firstNameValue = event.target.value;
+
+    if (firstNameValue != "" && firstNameValue.match(/^[a-zA-Z]+$/)) {
+        isValid = true;
+        pFirstNameError.innerText = "";
+    } else {
+        isValid = false;
+        pFirstNameError.innerText = "Veuillez renseigner un prénom";
+    }
+});
+
+let inputLastName = document.getElementById("lastName");
+let pLastNameError = document.getElementById("lastNameErrorMsg");
+
+inputLastName.addEventListener("input", function (event) {
+    event.preventDefault();
+    let lastNameValue = event.target.value;
+
+    if (lastNameValue != "" && lastNameValue.match(/^[a-zA-Z]+$/)) {
+        isValid = true;
+        pLastNameError.innerText = "";
+    } else {
+        isValid = false;
+        pLastNameError.innerText = "Veuillez renseigner un nom";
+    }
+});
+
+let inputAddress = document.getElementById("address");
+let pAddressError = document.getElementById("addressErrorMsg");
+
+inputAddress.addEventListener("input", function (event) {
+    event.preventDefault();
+    let addressValue = event.target.value;
+
+    if (addressValue != "") {
+        isValid = true;
+        pAddressError.innerText = "";
+    } else {
+        isValid = false;
+        pAddressError.innerText = "Veuillez remplir ce champ";
+    }
+});
+
+let inputCity = document.getElementById("city");
+let pCityError = document.getElementById("cityErrorMsg");
+
+inputCity.addEventListener("input", function (event) {
+    event.preventDefault();
+    let cityValue = event.target.value;
+
+    if (cityValue != "" && cityValue.match(/^[a-zA-Z]+$/)) {
+        isValid = true;
+        pCityError.innerText = "";
+    } else {
+        isValid = false;
+        pCityError.innerText = "Veuillez renseigner une ville";
+    }
+});
+
+let inputEmail = document.getElementById("email");
+let pEmailError = document.getElementById("emailErrorMsg");
+
+inputEmail.addEventListener("input", function (event) {
+    event.preventDefault();
+    let emailValue = event.target.value;
+
+    if (emailValue != "" && emailValue.match(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/)) {
+        isValid = true;
+        pEmailError.innerText = "";
+    } else {
+        isValid = false;
+        pEmailError.innerText = "Veuillez renseigner une adresse mail";
+    }
+});
+
+let inputCommander = document.getElementById("order");
+inputCommander.addEventListener("input", function (event) {
+    fetch('http://localhost:3000/api/products/order/',
+        method: “POST”,
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+        body: JSON.stringify(jsonBody)
+    )
+});
 
 
 
