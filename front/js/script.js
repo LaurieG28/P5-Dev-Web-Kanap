169ff7a87dbca3e56803fe14ae7509ca7764e2ad
Laurie Gazelle <laurie.gazelle@gmail.com>
2023-01-31 15:06:36 +0100

    clear script.JS

--- front/js/script.js
+++ front/js/script.js
@@ -8,7 +8,7 @@ fetch('http://localhost:3000/api/products/')
             return response.json();
         }
     })
-    // Attends la promesse du .json() et mets le resultat (le résultat transformé en json du coup) dans l'entrée de la fonction (reponse = résultat en json)
+    // Attends la promesse du .json() et mets le resultat (le résultat transformé en json) dans l'entrée de la fonction (reponse = résultat en json)
     .then(function (jsonPromise) {
         const items = document.getElementById("items");
         const products = jsonPromise;
@@ -37,71 +37,8 @@ fetch('http://localhost:3000/api/products/')
             items.appendChild(a);
         }
     })
-    // Si le fetch est en erreur (mauvaise url, back éteint, etc...) passe dans le catch (traduction pour attraper) et mets l'erreur en entrée de la fonction
+    // Si le fetch est en erreur (mauvaise url, back éteint, etc...) passe dans le catch (attraper) et mets l'erreur en entrée de la fonction
     .catch(function (err) {
         // Affiche l'erreur dans la console du navigateur web
         console.log(err);
     });
-
-/* pseudo-code /*
-
-plan :
-
-- on a p h3 et img enfants de article
-- article lui même enfant de a
-- dans ce tableau il y aura des objets dont un objet correspond à un tableau
-- dans cette boucle il y aura un tableau
-- a lui même enfant de section
-- dans section il y a 7 a 
-- donc dans section il va falloir créer une boucle
-- il faut récupérer section
-- faire un appel au back pour récupérer les canapés (fetch)
-
-*/
-
-/* application du plan
-
-fetch('http://localhost:3000/api/products/')
-.then (function(response){
-    if(response.ok){
-        return response.json();
-    }
-})
-
-.then(function(jsonPromise){
-    const items = document.getElementById("items");
-    const products = jsonPromise;
-    console.log(products);
-})
-
-.catch(function(error){
-    console.log(error);
-})
-
-après le fetch
-
-for(const product of products){
-
-    const a = document.createElement("a");
-    const article = document.createElement("article");
-    const image = document.createElement("img");
-    const name = document.createElement("h3");
-    const description = document.createElement("p");
-
-    a.href = "./product.html?id=" + product._id
-    image.src = product.imageUrl
-    image.alt = product.altTxt
-    name.classList.push("productName")
-    name.innerText = product.name
-    description.classList.push("productDescription")
-    description.innerText = product.description 
-
-    article.appendChild(productImage)
-    article.appendChild(productName);
-    article.appendChild(productDescription);
-    a.appendChild(article);
-    items.appendChild(a);  
-}
-
-
-*/
